e31e13fa9d09d6308eee5adbc47bdc31c10e9f1c
Ricardo Madriz <richin13@gmail.com>
2016-06-16 23:51:45 -0600

    Add some tests

---
+++ test/model.py
@@ -0,0 +1,17 @@
+import unittest
+from application.model.User import User
+
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.user = User('richin13', 'richin13@gmail.com', 'admin')
+        self.user.id = 1000351
+
+    def testRepr(self):
+        assert str(self.user) == "User <'richin13'>"
+
+    def test2(self):
+        assert self.user.id == 1000351
+
+    def test3(self):
+        print(self.user.password)
